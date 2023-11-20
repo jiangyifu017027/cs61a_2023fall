a82deab2c7bbe84104d12d4c6fb065172b3da788
jiangyifu017027 <2436537667@qq.com>
2023-11-20 16:43:19 +0800

    hw01-finish

--- hw01/hw01.py
+++ hw01/hw01.py
@@ -13,9 +13,9 @@ def a_plus_abs_b(a, b):
     3
     """
     if b < 0:
-        f = _____
+        f = sub
     else:
-        f = _____
+        f = add
     return f(a, b)
 
 def a_plus_abs_b_syntax_check():
@@ -42,7 +42,7 @@ def two_of_three(i, j, k):
     >>> two_of_three(5, 5, 5)
     50
     """
-    return _____
+    return i**2 + j**2 + k**2 - max(max(i, j), k)**2
 
 def two_of_three_syntax_check():
     """Check that your two_of_three code consists of nothing but a return statement.
@@ -65,7 +65,13 @@ def largest_factor(n):
     >>> largest_factor(13) # factor is 1 since 13 is prime
     1
     """
-    "*** YOUR CODE HERE ***"
+    result = 0
+    k = 1
+    while k < n:
+        if n % k == 0:
+            result = k
+        k = k + 1
+    return result
 
 
 def hailstone(n):
@@ -87,5 +93,14 @@ def hailstone(n):
     >>> b
     1
     """
-    "*** YOUR CODE HERE ***"
+    result = 1
+    while n != 1:
+        result = result + 1
+        print(n)
+        if n % 2 == 0:
+            n = n // 2
+        else:
+            n = n * 3 + 1
+    print(n)
 
+    return result
